0d509dc87abc99f6fef59a2950f6b77cf02b635b
DaniAkash <s.daniakash@gmail.com>
2023-03-18 09:57:03 +0530

    chore: added base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  base: "/beyond-virtual-dom",
 });
