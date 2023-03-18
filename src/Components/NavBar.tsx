a83c35131e07ad53d8d278059b7ad3a143d57cd2
DaniAkash <s.daniakash@gmail.com>
2023-03-18 15:00:43 +0530

    feat: added video links

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -75,11 +75,7 @@ export const NavBar = ({ activeUrl }: { activeUrl: string }) => {
                         className={classNames(
                           item.current
                             ? "bg-gray-900 text-white"
-                            : `text-transparent ${
-                                ["Slides"].includes(item.name)
-                                  ? "text-gray-100"
-                                  : ""
-                              } hover:bg-gray-700 hover:text-white`,
+                            : `text-gray-100 hover:bg-gray-700 hover:text-white`,
                           "rounded-md px-3 py-2 text-sm font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}
