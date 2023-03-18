7f871ddeec694fdc6383ea6158cdcd224c528d7c
DaniAkash <s.daniakash@gmail.com>
2023-03-18 09:34:06 +0530

    fix: deployment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/reveal.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
